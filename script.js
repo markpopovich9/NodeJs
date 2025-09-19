ebb7c9923ac1866e4c61c6b2675801ccd024311f
markpopovich9 <betmen070809@gmail.com>
2025-09-19 23:28:18 +0300

    dz

--- script.js
+++ script.js
@@ -1,23 +1,4 @@
-// const express = require("express");
-// const moment = require("moment"); 
-// const HOST = "127.0.0.1";
-// const PORT = 8000;
 
-
-// function getTime() {
-//   return moment().format("YYYY/MM/DD HH:mm:ss");
-// }
-
-
-// express.get("/timestamp", (req, res) => {
-//   const Time = getTime();
-//   res.json({ timestamp: Time });
-// });
-
-// // Запуск сервера
-// express.listen(PORT, () => {
-//   console.log(`Сервер запущен: http://${HOST}:${PORT}`);
-// });
 let moment = require("moment")()
 const express = require("express")()
 const HOST = "127.0.0.1"
