b30ba8e889b3b7e5ffc5a53a129ea1ff7ae2a02a
markpopovich9 <betmen070809@gmail.com>
2025-09-26 22:38:59 +0300

    Mark

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const fs = require("fs");
 const path = require("path");
 
 const app = express();
-const HOST = "127.0.0.1"
+const HOST = "127.0.0.1";
 const PORT = 8000;
 
 function getdate() {
@@ -14,17 +14,51 @@ function getdate() {
 }
 
 app.get("/timestamp", (req, res) => {
-  res.json({ timestamp: getdate()});
+  res.json({ timestamp: getdate() });
 });
 
 app.get("/posts", (req, res) => {
   const PathToFile = path.join(__dirname, "products.json");
   fs.readFile(PathToFile, "utf-8", (err, data) => {
-    res.json(JSON.parse(data));
+    if (err) {
+      return res.status(500).json({ error: "Ошибка чтения файла" });
+    }
+
+    let posts = JSON.parse(data);
+
+    let  skip = req.query.skip;
+    let take = req.query.take;
+    if ( isNaN(skip)) {
+      return res.status(400).json({ error: "skip должен быть числом" });
+    }
+    if (isNaN(take)) {
+      return res.status(400).json({ error: "take должен быть числом" });
+    }
+
+
+
+    posts = posts.slice(skip, take);
+
+    res.json(posts);
   });
 });
 
 
-app.listen(PORT,HOST, () =>{ 
-    console.log(`http://${HOST}:${PORT}`)
-})
+app.get("/posts/:id", (req, res) => {
+  const PathToFile = path.join(__dirname, "products.json");
+  fs.readFile(PathToFile, "utf-8", (err, data) => {
+    if (err) {
+      return res.status(500).json({ error: "Ошибка чтения файла" });
+    }
+
+    const posts = JSON.parse(data);
+    const id = req.params.id;
+
+
+    res.json(post);
+  });
+});
+
+app.listen(PORT, HOST, () => {
+  console.log(`http://${HOST}:${PORT}`);
+});
